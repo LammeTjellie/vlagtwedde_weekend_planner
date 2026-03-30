ecd3190c02cef6a8a19aaf005f6256add8f3b4c8
LammeTjellie <schoenmaker.rik@gmail.com>
2026-03-30 12:59:05 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig, loadEnv} from 'vite';
@@ -7,7 +6,7 @@ export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
     base: '/LammeTjellie/vlagtwedde_weekend_planner',  // ← jouw exacte reponaam hier
-    plugins: [react(), tailwindcss()],
+    plugins: [react(),
     define: {
       'VITE_GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
     },
